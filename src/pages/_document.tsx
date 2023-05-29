08aa933e65bb39c300cdd7fb6cb7e051b5a4f6b7
Marcos Hirazawa <hirazawa.marcos@gmail.com>
2023-05-29 20:15:14 -0300

    _doc fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,7 +42,7 @@ export default class MyDocument extends Document {
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
-            crossOrigin="true"
+            crossOrigin="anonymous"
           ></link>
           <link
             href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,600;0,700;1,200&display=swap"
